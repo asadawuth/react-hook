744bec82994e2ccc2fe40476a26443d847b24019
asadawuth <taodewy@gmail.com>
2024-01-11 13:02:20 +0700

    effect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [category, setCategory] = useState("");
+  const [count, setCount] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  async function fetchLists() {
+    const BASE_URL = "http://jsonplaceholder.typicode.com";
+    try {
+      let response = await fetch(`${BASE_URL}/${category}`, {
+        method: "GET",
+      });
+      let data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // ทุกครั้งที่ State เปลี่ยน useeffect จะทำงาน
+  /*
+  useEffect(() => {
+    console.log("Even body");
+  });
+  */
+  // every render,rerender
+  // Syntax : useEffect(setUp,depenciesArray)
+
+  /*
+  useEffect(() => {
+    console.log("Effect count");
+  }, [count]);
+*/
+  // 1. console.log("1"); 2. state 3. useEffect 4. state มาก่อน  useEffect ถึงทำงาน
+  //DEFINE Fn
+  // CALL FN
+  useEffect(() => {
+    console.log(`useEffect ${category}`);
+    //if (category !== "") fetchLists();
+  }, [category]);
+
+  return (
+    <div>
+      <h1>Effect-hook : {category || "not-select"}</h1>
+      <div className="botton__group">
+        <button onClick={() => setCategory("posts")}>posts</button>
+        <button onClick={() => setCategory("photos")}>photos</button>
+        <button onClick={() => setCategory("todos")}>todos</button>
+        <button onClick={() => setCategory("users")}>users</button>
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <div>{count}</div>
+        <button onClick={() => setCount((c) => c - 1)}>-</button>
+      </div>
+
+      <ul className="lists">
+        <li className="lists__item">item-1</li>
+      </ul>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
